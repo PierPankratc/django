5cce5b953e3be6e49593544e407727d3dd917857
Petr <peterpankratc@gmail.com>
2026-08-16 17:46:48 +0300

    +

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import get_object_or_404, render, redirect
+from phones.management.commands.import_phones import Command
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort_param = request.GET.get('sort')
     template = 'catalog.html'
-    context = {}
+    if sort_param == 'name':
+        phones = Phone.objects.all().order_by('slug')
+    elif sort_param == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_param == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = get_object_or_404(Phone, slug=slug)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone}
     return render(request, template, context)

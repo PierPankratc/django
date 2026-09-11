9adf98678209ef3059dfe570ddd52f344624c081
Petr <peterpankratc@gmail.com>
2026-09-11 17:58:16 +0300

    add models

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length=150, null=True)
+
+class Measurement(models.Model):
+    id = models.IntegerField(primary_key=True)
+    t = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
